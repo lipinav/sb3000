5f6861ec0e00c85b4da48af263bee6f630f2c60a
lipinav <lipinavpost@gmail.com>
2021-10-22 01:02:22 +0300

    fixup! 4. fix: more components and create data array for Card component

---
+++ src/shared/CardsList/Card/Title/Title.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './title.css';
+
+interface ITitle {
+  title: string;
+}
+
+export function Title(props: ITitle) {
+  return (
+    <h2 className={styles.title}>
+      <a href="#post-url" className={styles.postLink}>
+        {props.title}
+        {/*Следует отметить, что новая модель организационной деятельности...*/}
+      </a>
+    </h2>
+  );
+}
