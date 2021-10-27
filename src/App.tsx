1b1313ff758775c2e3ec9b6789bcfdd1995ead23
lipinav <lipinavpost@gmail.com>
2021-10-27 19:00:17 +0300

    5.2.hooks

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,22 @@ import { Layout } from './shared/Layout';
 import { Header } from './shared/Header';
 import { Content } from './shared/Content';
 import { CardsList } from './shared/CardsList';
+import { getValue } from './utils/react/getValue';
+import { MyHooks, useIsMounted } from '../notes/5.2.hooks';
 
 function AppComponent() {
+  // const [isVisible, setIsVisible] = React.useState(false);
+  const [title, setTitle] = React.useState('');
+  const [isVisible] = useIsMounted();
+
   return(
     <Layout>
       <Header />
       <Content>
         <CardsList />
+        {/*<button onClick={() => setIsVisible(!isVisible)}>change me!</button>*/}
+        <input type="text" onChange={getValue(setTitle)} />
+        {isVisible && <MyHooks title={title} id="11"/>}
       </Content>
     </Layout>
   );
