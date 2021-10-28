477b0ec308f251aa612e9c35d3d342ea4b9bee8b
lipinav <lipinavpost@gmail.com>
2021-10-28 15:40:25 +0300

    5.3. map-reduce-react remove and add new elements

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Header } from './shared/Header';
 import { Content } from './shared/Content';
 import { CardsList } from './shared/CardsList';
 import { generateId } from './utils/react/generateRandomIndex';
-// import { generateRandomString } from './utils/react/generateRandomIndex';
+import { generateRandomString } from './utils/react/generateRandomIndex';
 import { MyList } from './shared/GenericList';
 import { merge } from './utils/js/merge';
 
@@ -20,15 +20,20 @@ const LIST = [
 ].map(generateId)
 
 function AppComponent() {
+  const [list, setList] = React.useState(LIST);
   const handleItemClick = (id: string) => {
-    console.log(id);
+    setList(list.filter((item) => item.id !== id));
+  }
+  const handleAdd = () => {
+    setList(list.concat(generateId({title: generateRandomString()})));
   }
   return(
     <Layout>
       <Header />
       <Content>
-        <CardsList list={LIST.map(merge({ onClick: handleItemClick}))}/>
-        <MyList list={LIST.map(merge({ onClick: handleItemClick }))}/>
+        <CardsList list={list.map(merge({ onClick: handleItemClick}))}/>
+        <button onClick={handleAdd}>Add element</button>
+        <MyList list={list.map(merge({ onClick: handleItemClick }))}/>
       </Content>
     </Layout>
   );
