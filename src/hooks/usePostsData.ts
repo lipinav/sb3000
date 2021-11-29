cf38a23ed3fd9c6690c7cbc139d0ea041e085a8d
lipinav <lipinavpost@gmail.com>
2021-11-29 17:08:47 +0300

    fixup! 8.4.context

--- src/hooks/usePostsData.ts
+++ src/hooks/usePostsData.ts
@@ -2,26 +2,38 @@ import { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import { tokenContext } from '../shared/context/tokenContext';
 
-interface IPosts {
-  name?: string;
+interface IPostsData {
+  after?: string | null,
+  dist?: number,
+  modhash?: string | null,
+  geo_filter?: string | null,
+  children?: Array<object>
 }
 
-export function usePosts () {
-  const [posts, setPosts] = useState<IPosts>({});
+export function useBestPostsData () {
+  const [posts, setPostsData] = useState<IPostsData>({});
   const token = useContext(tokenContext);
+
   useEffect(() => {
-    axios.get(
-      'https://oauth.reddit.com/api/v1/me',
-      {  // config
-        headers: { Authorization: `bearer ${token}` }
-      }
-    )
-      .then((resp) => {
-          console.log('resp.data: ', resp.data);
-          const postsData = resp.data;
-          setPosts(postsData)
-      })
-      .catch((err) => {console.log('Axios err: ', err)});
+    if (token !== '' && typeof(token) !== 'undefined') {
+      axios.get(
+        'https://oauth.reddit.com/best',
+        {  // config
+          headers: { Authorization: `bearer ${token}` }
+        }
+      )
+        .then((resp) => {
+            const postsData = resp.data;
+            console.log('[src/hooks/usePostsData.ts] useEffect resp: ', resp);
+            setPostsData({
+              after: postsData.data.after,
+              dist: postsData.data.dist,
+              modhash: postsData.data.modhash,
+              children: postsData.data.children
+            })
+        })
+        .catch((err) => {console.log('[src/hooks/usePostsData.ts] Axios err: ', err)});
+    }
   }, [token])
 
   return [posts]
--- src/shared/context/postsContext.tsx
+++ src/shared/context/postsContext.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { usePosts } from '../../hooks/usePostsData';
 
 interface IPostsContext {
+<<<<<<< Updated upstream:src/shared/context/postsContext.tsx
   name?: string
+=======
+  after?: string | null,
+  dist?: number,
+  modhash?: string | null,
+  geo_filter?: string | null,
+  children?: Array<object>
+>>>>>>> Stashed changes:src/shared/context/bestPostsContext.tsx
 }
 export const postsContext = React.createContext<IPostsContext>({});
 
