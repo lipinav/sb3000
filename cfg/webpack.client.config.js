15af59f794c0293e71dd38ce837516d369eda1ab
lipinav <54190787+lipinav@users.noreply.github.com>
2021-11-29 04:15:32 +0300

    7.api (#17)
    * fix: warning
    
    * fix: REDIRECT_URI

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -7,7 +7,12 @@ const IS_DEV = NODE_ENV === 'development';
 const IS_PROD = NODE_ENV === 'production';
 const GLOBAL_CSS_REGEXP = /\.global\.css$/;
 const DEV_PLUGINS = [ new CleanWebpackPlugin(), new HotModuleReplacementPlugin() ]
-const COMMON_PLUGINS = [ new DefinePlugin({'process.env.CLIENT_ID': `'${process.env.CLIENT_ID}'`})]
+const COMMON_PLUGINS = [ new DefinePlugin(
+  {
+    'process.env.CLIENT_ID': `'${process.env.CLIENT_ID}'`,
+    'process.env.REDIRECT_URI': `'${process.env.REDIRECT_URI}'`
+  }
+)]
 
 function setupDevtool() {
   if (IS_DEV) {
